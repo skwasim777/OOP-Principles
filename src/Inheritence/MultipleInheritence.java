c66b7c30be536332c64f3c12e31796c4d559f225
skwasim777 <wasimsk6061@gmail.com>
2023-10-22 18:42:55 +0530

    multiple inheritence

---
+++ src/Inheritence/MultipleInheritence.java
@@ -0,0 +1,16 @@
+package Inheritence;
+
+// multiple inheritence does not support java through classes
+//public class MultipleInheritence extends A,B{
+//	
+//}
+//class A{
+//	void msg() {
+//		
+//	}
+//}
+//class B{
+//	void msg() {
+//		
+//	}
+//}
